01c358eae8c230c8865d9a37a597ee6cd42a8a56
Kykeon <paulinoandresmiguel@gmail.com>
2022-04-22 21:34:16 -0400

    Add you may also like moving marquee

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { client, urlFor } from '../../lib/client';
 import { AiOutlineMinus, AiOutlinePlus, AiFillStar, AiOutlineStar } from 'react-icons/ai';
+import Product from '../../components/Product';
 
 function ProductDetails({ product, products }) {
   const { image, name, details, price } = product;
@@ -18,7 +19,7 @@ function ProductDetails({ product, products }) {
                         ))}
                   </div> */}
         </div>
-        <div className='product-details-desc'>
+        <div className='product-detail-desc'>
           <h1>{name}</h1>
           <div className='reviews'>
             <div>
@@ -29,21 +30,45 @@ function ProductDetails({ product, products }) {
               <AiOutlineStar />
             </div>
             <p>(20)</p>
-                  </div>
-                  <h4>Details: </h4>
-                  <p>{details}</p>
-                  <p className="price">${price}</p>
-                  <div className="quantity">
-                      <h3>Quantity:</h3>
-                      <p className='quantity-desc'>
-                          <span className='minus' onClick=''><AiOutlineMinus /></span>
-                          <span className='num' onClick=''>0</span>
-                          <span className='plus' onClick=''><AiOutlinePlus /></span>
-
-                      </p>
-                  </div>
+          </div>
+          <h4>Details: </h4>
+          <p>{details}</p>
+          <p className='price'>${price}</p>
+          <div className='quantity'>
+            <h3>Quantity:</h3>
+            <p className='quantity-desc'>
+              <span className='minus' onClick=''>
+                <AiOutlineMinus />
+              </span>
+              <span className='num' onClick=''>
+                0
+              </span>
+              <span className='plus' onClick=''>
+                <AiOutlinePlus />
+              </span>
+            </p>
+          </div>
+          <div className='buttons'>
+            <button type='button' className='add-to-cart' onClick=''>
+              Add to Cart
+            </button>
+            <button type='button' className='buy-now' onClick=''>
+              Buy Now
+            </button>
+          </div>
         </div>
-      </div>
+          </div>
+          
+          <div className='maylike-products-wrapper'>
+              <h2>You may also like</h2>
+              <div className="marquee">
+                  <div className="maylike-products-container track">
+                      {products.map((item) => (
+                          < Product key={item._id} product={item} />
+                      ))}
+                  </div>
+              </div>
+              </div>
     </div>
   );
 }
