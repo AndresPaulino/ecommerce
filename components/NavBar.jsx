af0ef5b8f881ed16689f18181b06010e055e342b
Kykeon <paulinoandresmiguel@gmail.com>
2022-04-22 21:26:09 -0400

    Add slug.js page and getStaticPaths

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from 'react';
+import Link from 'next/link';
+import { AiOutlineShopping } from 'react-icons/ai';
 
 function NavBar() {
   return (
-    <div>NavBar</div>
-  )
+    <div className='navbar-container'>
+      <p className='logo'>
+        <Link href='/'>BassBoost Headphones</Link>
+      </p>
+
+      <button type='button' className='cart-icon' onClick=''>
+        <AiOutlineShopping />
+        <span className="cart-item-qty">1</span>
+      </button>
+    </div>
+  );
 }
 
-export default NavBar
+export default NavBar;
