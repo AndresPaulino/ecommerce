ddcde6925b8dceea4d5c93021b8a3eafdbe1b68d
Kykeon <paulinoandresmiguel@gmail.com>
2022-04-22 22:03:18 -0400

    Add decrease and increase quantity state changes

--- pages/_app.js
+++ pages/_app.js
@@ -9,7 +9,7 @@ function MyApp({ Component, pageProps }) {
   return (
     <StateContext>
       <Layout>
-        <Toast />
+        <Toaster />
         <Component {...pageProps} />
       </Layout>
     </StateContext>
