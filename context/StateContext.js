5109cbcffcd6ca6f8bdadc1570abe23e031d564c
Kykeon <paulinoandresmiguel@gmail.com>
2022-04-22 22:21:54 -0400

    Add showCart functionality to navBar component

--- context/StateContext.js
+++ context/StateContext.js
@@ -5,10 +5,29 @@ const Context = createContext();
 
 export const StateContext = ({ children }) => {
   const [showCart, setShowCart] = useState(false);
-  const [cartItems, setCartItems] = useState();
+  const [cartItems, setCartItems] = useState([]);
   const [totalPrice, setTotalPrice] = useState(0);
   const [totalQuantity, setTotalQuantity] = useState(0);
   const [qty, setQty] = useState(1);
+
+  const onAdd = (product, quantity) => {
+    const checkProductInCart = cartItems.find((item) => item.product._id === product._id);
+
+    if (checkProductInCart) {
+      setTotalPrice((previousTotalPrice) => previousTotalPrice + product.price * quantity);
+      setTotalQuantity((previousTotalQuantity) => previousTotalQuantity + quantity);
+
+      const updatedCartItems = cartItems.map((cartProduct) => {
+        if (cartProduct._id === product._id) return { ...cartProduct, quantity: cartProduct.quantity + quantity };
+      });
+      setCartItems(updatedCartItems);
+    } else {
+      product.quantity = quantity;
+
+      setCartItems([...cartItems, { ...product }]);
+    }
+    toast.success(`${qty} ${product.name} added to cart.`);
+  };
 
   const incQty = () => {
     setQty((prevQty) => prevQty + 1);
@@ -32,6 +51,8 @@ export const StateContext = ({ children }) => {
         qty,
         incQty,
         decQty,
+        onAdd,
+        setShowCart,
       }}
     >
       {children}
