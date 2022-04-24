28e74e01eef380476f372cf55c93376bb9fbf862
Kykeon <paulinoandresmiguel@gmail.com>
2022-04-23 20:05:11 -0400

    Link all footer icons

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,17 +1,22 @@
 import React from 'react'
-import { AiFillInstagram, AiOutlineTwitter } from 'react-icons/ai'
+import { AiFillGithub, AiFillMessage, AiFillLinkedin } from 'react-icons/ai'
 
 
 function Footer() {
   return (
     <div className='footer-container'>
-      <p>2022 Bassboost Headphones All right reserved</p>
-      <p className="icons">
-        <AiFillInstagram />
-        <AiOutlineTwitter />
+      <p>2022 HyperFize Technologies - All right reserved</p>
+      <p className='icons'>
+        <a href='https://github.com/AndresPaulino'>
+          <AiFillGithub />
+        </a>
+        <a href='mailto:paulinoandresmiguel@gmail.com'>
+          <AiFillMessage />
+        </a>
+        <a href='https://www.linkedin.com/in/andrespaulino/'><AiFillLinkedin /></a>
       </p>
     </div>
-  )
+  );
 }
 
 export default Footer
